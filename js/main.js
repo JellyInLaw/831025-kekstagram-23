2f4c130757f738bf4cc66dad08c30d9d5842e075
Maxim Shashurin <waiyyshik@gmail.com>
2021-07-15 00:33:04 +0500

    some fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-import {photoDescriptions} from './utils/data.js';
 import './mini-picture.js';
 import './fullscreen-picture.js';
 import './form.js';
 import './image-editing.js';
-
-photoDescriptions.length;//для автопроверки npm test
-
 
